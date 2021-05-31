8f6b07ebaa50caa8a96ed052c38130b7030fb380
du2lee <dh151446@gmail.com>
2021-05-31 11:31:09 +0900

    bfs

---
+++ bfs.py
@@ -0,0 +1,30 @@
+def bfs(graph, start_node):
+    visited, need_visit = list(), list()
+
+    need_visit.append(start_node)
+
+    while need_visit:
+        node = need_visit.pop(0)
+        if node not in visited:
+            visited.append(node)
+            need_visit.extend(graph[node])
+
+    return visited
+
+
+graph = dict()                              #We can use dict to make graph in python
+
+graph['A'] = ['B', 'C']                     #example data
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+
+graph
+print(bfs(graph, 'A'))
